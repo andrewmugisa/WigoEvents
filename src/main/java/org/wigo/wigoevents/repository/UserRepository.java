e84ac933ba098522bc0fa76c180bf7b3d81f7e39
Andrew.M <mugisaandrew635@gmail.com>
2026-06-09 23:23:35 -0400

    Rename myday -> wigoevents

---
+++ src/main/java/org/wigo/wigoevents/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package org.wigo.wigoevents.repository;
+
+import org.wigo.wigoevents.model.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByVerificationCode(String verificationCode);
+    Optional<UserEntity> findByUsername(String username);
+}
