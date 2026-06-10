1e06525f5ad691717fdb92d2dbe3f66ad9cd3b7c
Andrew.M <mugisaandrew635@gmail.com>
2026-06-09 23:45:29 -0400

    Fix: correct UserDetailsServiceAutoConfiguration package for Spring Boot 4

--- src/main/java/org/wigo/wigoevents/WigoEventsApplication.java
+++ src/main/java/org/wigo/wigoevents/WigoEventsApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication(excludeName = {
         "org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration",
         "org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration",
-        "org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration",
-        "org.springframework.boot.security.autoconfigure.servlet.UserDetailsServiceAutoConfiguration"
+        "org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration"
 })
 public class WigoEventsApplication {
     public static void main(String[] args) {
