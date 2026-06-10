53c53cab56f30675749c0b19fccd2da2eaa323e6
Andrew.M <mugisaandrew635@gmail.com>
2026-06-09 23:23:42 -0400

    Rename myday -> wigoevents

---
+++ src/test/java/org/wigo/wigoevents/WigoEventsApplicationTests.java
@@ -0,0 +1,11 @@
+package org.wigo.wigoevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WigoEventsApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
