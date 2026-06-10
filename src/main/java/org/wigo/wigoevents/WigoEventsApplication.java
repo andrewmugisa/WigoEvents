8d76e180cc203daf6a2e87ef87d536dc9ea11470
Andrew.M <mugisaandrew635@gmail.com>
2026-06-09 23:41:19 -0400

    Fix: correct Spring Boot 4 security exclude packages

--- src/main/java/org/wigo/wigoevents/WigoEventsApplication.java
+++ src/main/java/org/wigo/wigoevents/WigoEventsApplication.java
@@ -2,11 +2,13 @@ package org.wigo.wigoevents;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
+import org.springframework.boot.security.autoconfigure.web.servlet.ServletWebSecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = {
         SecurityAutoConfiguration.class,
+        ServletWebSecurityAutoConfiguration.class,
         UserDetailsServiceAutoConfiguration.class
 })
 public class WigoEventsApplication {
