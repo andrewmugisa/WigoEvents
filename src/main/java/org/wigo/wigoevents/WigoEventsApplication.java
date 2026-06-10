cd61559937a8b459b1f59b4bde9ec53162ef7e73
Andrew.M <mugisaandrew635@gmail.com>
2026-06-09 23:23:33 -0400

    Rename myday -> wigoevents

---
+++ src/main/java/org/wigo/wigoevents/WigoEventsApplication.java
@@ -0,0 +1,11 @@
+package org.wigo.wigoevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WigoEventsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WigoEventsApplication.class, args);
+    }
+}
